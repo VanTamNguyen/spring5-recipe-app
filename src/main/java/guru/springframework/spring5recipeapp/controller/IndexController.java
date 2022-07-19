24bcfa573b3b285cf4ae3b27abee08606b9ef447
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2022-07-19 23:37:53 +0700

    Model and view

--- src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
@@ -3,8 +3,14 @@ package guru.springframework.spring5recipeapp.controller;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -19,5 +25,18 @@ public class IndexController {
     public String indexPage(Model model) {
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
+    }
+
+    @GetMapping("/model_map")
+    public String modalMap(ModelMap modelMap) {
+        modelMap.addAttribute("recipes", recipeService.getRecipes());
+        return "index";
+    }
+
+    @GetMapping("/model_and_view")
+    public ModelAndView modelAndView() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("recipes", recipeService.getRecipes());
+        return new ModelAndView("index", model);
     }
 }
